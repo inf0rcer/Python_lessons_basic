b87315663b8c9d078f048e57a3a136e657cb4c45
inf0rcer <nikwind119@gmail.com>
2019-10-06 21:58:46 +0300

    Все изи,2 нормала

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -19,8 +19,21 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+#1)используя re
+import re
+res = re.findall(r'[a-z]+', line)
+print(res)
+#2)не используя re
+symbols_up = list(map(lambda x: chr(x), list(range(65,91))))
+line_list = list(line)
+for i, el in enumerate(line_list[:]):
+    for el2 in symbols_up:
+        if el == el2:
+            line_list[i] = ' '
+str_sep = ''.join(line_list).split(' ')
 
-
+res_str = [i for i in str_sep if i != '']
+print(res_str)
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
 # два символа в нижнем регистре, а справа - два символа в верхнем регистре.
@@ -44,7 +57,45 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'uXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQN'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+#1)используя re
+res = re.findall(r'[a-z]{2}([A-Z]+)[A-Z]{2}', line_2)
+print(res)
+#2)не используя re
+symbols_up = list(map(lambda x: chr(x), list(range(65, 91))))
+symbols_down = list(map(lambda x: chr(x), list(range(97, 123))))
+line_list2 = list(line_2)
 
+list1 = []
+i = len(line_list2) - 1
+
+while i >= 0:
+    if line_list2[i] in symbols_down:
+         list1.append(line_list2[i])
+    elif line_list2[i] in symbols_up and i <= len(line_list2) - 3 and line_list2[i+1] in symbols_up and line_list2[i+2] in symbols_up:
+        list1.append(line_list2[i])
+    else:
+        list1.append(' ')
+    i -= 1
+list1.reverse()
+
+i = 0
+list2 = []
+
+while i <= len(list1)-1:
+    if list1[i] in symbols_down:
+        upper = True
+    if list1[i] in symbols_up and list1[i-1] in symbols_down and list1[i-2] in symbols_down:
+        list2.append(list1[i])
+        upper = False
+    elif list1[i] in symbols_up and upper == False:
+        list2.append(list1[i])
+    else:
+        list2.append(' ')
+    i += 1
+string=''.join(list2).split(' ')
+
+res_str_2 = [i for i in string if i != '']
+print(res_str_2)
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
