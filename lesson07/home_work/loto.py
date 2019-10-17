2f0d2b77281bc951cdffffbfab2f3e23a3f9d285
inf0rcer <nikwind119@gmail.com>
2019-10-17 13:21:20 +0300

    сделал лото

--- lesson07/home_work/loto.py
+++ lesson07/home_work/loto.py
@@ -57,3 +57,99 @@
 модуль random: http://docs.python.org/3/library/random.html
 
 """
+from random import randint
+
+
+class Card:
+	def __init__(self, name):
+		barrel_bag = [x for x in range(1, 91)]
+		self.card = [
+		    __class__.gen_string(barrel_bag),
+		    __class__.gen_string(barrel_bag),
+		    __class__.gen_string(barrel_bag)
+		]
+		self.name = name
+		self.count_barrel = 15
+
+	@staticmethod
+	def gen_string(barrel_bag):
+		string = ['' for _ in range(9)]
+		for x in range(8, 3, -1):
+			digit = randint(0, x)
+			while string[digit] != '':
+				digit += 1
+			string[digit] = barrel_bag.pop(randint(0, len(barrel_bag) - 1))
+		return string
+
+	def __str__(self):
+		rez = '{:-^26}\n'.format(self.name)
+		for x in range(3):
+			rez += '{:>2} {:>2} {:>2} {:>2} {:>2} {:>2} {:>2} {:>2} {:>2}'\
+                    .format(*self.card[x]) + '\n'
+		return rez + '--------------------------'
+
+
+user = Card(' Пользователь ')
+computer = Card(' Компьютер ')
+
+barrel_bag = [x for x in range(1, 91)]
+while True:
+	if len(barrel_bag) < 1:
+		print('Сумка с боченками пуста.\n'
+		      'у пользователя осталось {} чисел,\n'
+		      'у компьютера осталось {} чисел.'.format(
+		          user.count_barrel, computer.count_barrel))
+		break
+	barrel = barrel_bag.pop(randint(0, len(barrel_bag) - 1))
+	print('\nНовый боченок: {} (осталось {})'.format(barrel, len(barrel_bag)))
+	print(computer)
+	print(user)
+	action = input('Зачеркнуть цифру? (y/n)')
+	action = action.lower()
+	while len(action) == 0 or action not in 'yn':
+		print('\n\nНезивестный ввод\n')
+		print('Новый боченок: {} (осталось {})'.format(barrel, len(bag)))
+		print(computer)
+		print(user)
+		action = input('Зачеркнуть цифру? (y/n)')
+		action = action.lower()
+
+
+	if action == 'y':
+		test = False
+		for x in range(3):
+			if barrel in user.card[x]:
+				test = True
+				user.card[x][user.card[x].index(barrel)] = '-'
+				user.count_barrel -= 1
+			if barrel in computer.card[x]:
+				computer.card[x][computer.card[x].index(barrel)] = '-'
+				computer.count_barrel -= 1
+		if test:
+			if user.count_barrel < 1:
+				print('Пользователь победил!')
+				break
+			elif computer.count_barrel < 1:
+				print('Компьютер победил!')
+				break
+		else:
+			print('Пользователь проиграл! Такого числа нет на карточке')
+			break
+	elif action == 'n':
+		test = False
+		for x in range(3):
+			if barrel in user.card[x]:
+				print('Пользователь проиграл! Такое число есть на карточке')
+				test = True
+				break
+			if barrel in computer.card[x]:
+				computer.card[x][computer.card[x].index(barrel)] = '-'
+				computer.count_barrel -= 1
+		if test:
+			break
+		if user.count_barrel < 1:
+			print('Пользователь выиграл!')
+			break
+		elif computer.count_barrel < 1:
+			print('Компьютер выиграл!')
+			break
